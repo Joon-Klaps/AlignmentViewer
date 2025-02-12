84a740da70915e08852dd9f6de96cecaafbba8fa
Joon-Klaps <joon.klaps@kuleuven.be>
2025-02-12 13:45:10 +0100

    refactor to follow python structure

--- src/AlignmentViewer/__init__.py
+++ src/AlignmentViewer/__init__.py
@@ -1,8 +1,4 @@
-from .viewer import AlignmentViewer, ColorScheme, DisplayConfig, SequenceReader
+from .AlignmentViewer import AlignmentViewer, ColorScheme, DisplayConfig, SequenceReader
 
 __all__ = ['AlignmentViewer', 'ColorScheme', 'DisplayConfig', 'SequenceReader']
 
-try:
-    from ._version import version as __version__
-except ImportError:
-    __version__ = "unknown"
